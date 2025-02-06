d7915a1d896b62c66329c42907dce1f0e8a103c7
pgouasmi <pgouasmi@student.42lyon.fr>
2025-02-06 16:36:34 +0100

    [FEATURE] p2 OK but middleware not automatically applying

--- p2/scripts/provision_server.sh
+++ p2/scripts/provision_server.sh
@@ -3,4 +3,9 @@
 sudo apt-get update
 sudo apt-get install -y curl net-tools
 curl -sfL https://get.k3s.io | sh -s server --flannel-iface=eth1 --write-kubeconfig-mode 644
+
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+
 kubectl apply -f /vagrant/confs
+
+alias k="kubectl"
