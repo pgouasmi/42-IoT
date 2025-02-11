3a70f2378fa60130105f4f70585db61c1a15a1be
flash <flash@vbox.42lyon.fr>
2025-02-11 15:14:56 +0100

    [CLEAN]

--- p2/scripts/provision_server.sh
+++ p2/scripts/provision_server.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 sudo apt-get install -y curl net-tools
 curl -sfL https://get.k3s.io | sh -s server --flannel-iface=eth1 --write-kubeconfig-mode 644
 
-# kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
 
 kubectl apply -f /vagrant/confs
 
